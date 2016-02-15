68f4f8f9aa7aba68f7b2ad396e03706fce973ad6
Dave Shah <dave.shah@genomoncology.com>
2016-02-14 20:40:16 -0500

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,29 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :rumbl, Rumbl.Endpoint,
+  url: [host: "localhost"],
+  root: Path.dirname(__DIR__),
+  secret_key_base: "io+y8+Fy38t/URcoYZ2VMWOsSwfT4OY/8ovTxd/qr07P02BEXMFO2ov601EhwNwS",
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Rumbl.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+# Configure phoenix generators
+config :phoenix, :generators,
+  migration: true,
+  binary_id: false
