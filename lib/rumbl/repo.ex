021c3a9b8966005d68847d905ccc466a67a1b19f
Dave Shah <dave.shah@genomoncology.com>
2016-02-14 21:06:33 -0500

    Add user model and hardcoded repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,25 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+#  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Dave", username: "daveshah", password: "high-fives"},
+     %Rumbl.User{id: "2", name: "Brave", username: "braveshah", password: "high-dives"},
+     %Rumbl.User{id: "3", name: "Rave", username: "Raveshah", password: "high-lives"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map -> 
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end) 
+    end
+  end
+
 end
