fe17ca80f88a8e6c202582f31e0611d373de5ca6
Dave Shah <dave.shah@genomoncology.com>
2016-02-22 06:03:53 -0500

    Add create - odd that redirect didn't work

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -17,4 +17,14 @@ defmodule Rumbl.UserController do
     render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{ "user" => user_params }) do
+    changeset = User.changeset(%User{}, user_params)
+    render conn, "new.html", changeset: changeset
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
+  end
+
 end
