7feafd4af8eee41829f2a89da6c4e1b3ea4d32cb
Dave Shah <david.a.shah@gmail.com>
2016-04-12 05:38:56 -0400

    WIP - 126

--- web/models/user.ex
+++ web/models/user.ex
@@ -19,6 +19,7 @@ defmodule Rumbl.User do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 
   def registration_changeset(model, params) do
