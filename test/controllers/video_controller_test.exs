2177e74a47308c2f80db9d59ec68e50100add7dc
Dave Shah <david.a.shah@gmail.com>
2016-05-01 09:51:14 -0400

    tag tests requring login

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,6 +1,29 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "dave"
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "Airplane")
+    other_user_video = insert_video(insert_user(username: "not dave"),
+                                     title: "Airplane 2")
+
+    conn = get conn, video_path(conn, :index)
+
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_user_video.title)
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,11 +1,12 @@
 defmodule Rumbl.TestHelpers do
-  alias Rumble.Repo
+  alias Rumbl.Repo
 
   def insert_user(attrs \\ %{}) do
     changes = Dict.merge(%{
       name: "Some User",
       username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
       password: "supersecret",
+      password_confirmation: "supersecret",
     }, attrs)
 
     %Rumbl.User{}
