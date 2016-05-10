e517d981f69eb9e5cb7d993a8ae9cce0e15d33f7
Dave Shah <david.a.shah@gmail.com>
2016-05-09 20:52:38 -0400

    Add watch

--- config/config.exs
+++ config/config.exs
@@ -27,3 +27,7 @@ import_config "#{Mix.env}.exs"
 config :phoenix, :generators,
   migration: true,
   binary_id: false
+
+# Configure haml
+config :phoenix, :template_engines,
+  haml: PhoenixHaml.Engine
