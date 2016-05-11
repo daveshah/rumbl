6a5fc1499792da4ed4c66e40eb22883df5f1aa52
Dave Shah <david.a.shah@gmail.com>
2016-05-11 15:53:02 -0400

    Simple chat is all wired up

---
+++ web/channels/video_channel.ex
@@ -0,0 +1,17 @@
+defmodule Rumbl.VideoChannel do
+  use Rumbl.Web, :channel
+
+  def join("videos:" <> video_id, params, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
+end
